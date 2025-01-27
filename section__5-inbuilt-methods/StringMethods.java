5048ed71d962ead7ec48626f98129cb7480645dd
Neeraj Shankar <neeshank@cisco.com>
2025-01-27 19:09:52 +0530

    re structured folders and added depth analysis on length() method.

---
+++ section__5-inbuilt-methods/StringMethods.java
@@ -0,0 +1,11 @@
+class StringBuiltIn{
+
+    public static void commonMethods(String s){
+
+        // getting the length of the string.
+        int len = s.length(); 
+    }
+}
+public class StringMethods {
+    
+}
